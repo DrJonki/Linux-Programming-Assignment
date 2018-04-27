18229200d7cd4a543d2ac0f9f36e5235f0c806b2
Joona Tiinanen <joona_tiinanen@hotmail.com>
2018-04-27 12:47:41 +0300

    Removed unneede mutex from p2t2

--- OSA2/T2/main.c
+++ OSA2/T2/main.c
@@ -2,15 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <pthread.h>
 
-pthread_mutex_t mutex;
 int counter = 1;
 
 void SIGINTHandler(int signum)
 {
-  pthread_mutex_lock(&mutex);
-
   int sum = 0;
   for (; counter > 0; --counter) {
     sum += counter;
@@ -18,23 +14,15 @@ void SIGINTHandler(int signum)
 
   printf("Sum: %d\n", sum);
 
-  pthread_mutex_unlock(&mutex);
-
   exit(EXIT_SUCCESS);
 }
 
 int main()
 {
-  pthread_mutex_init(&mutex, NULL);
-
   signal(SIGINT, SIGINTHandler);
 
   for (;; ++counter) {
-    pthread_mutex_lock(&mutex);
-
     printf("Counter: %d\n", counter);
-
-    pthread_mutex_unlock(&mutex);
 
     sleep(1);
   }
